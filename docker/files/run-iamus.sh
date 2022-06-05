72df9e67b3efdefb8bd2d61923f6f7fd12cd7b22
xiejiang <z.kanie@gmail.com>
2022-06-05 13:20:57 +0900

    Update run-iamus.sh

--- docker/files/run-iamus.sh
+++ docker/files/run-iamus.sh
@@ -5,9 +5,7 @@ BASE=/home/cadia/Iamus
 # Default location for logs is in the Iamus sub-directory
 LOGDIR=${BASE}/Logs
 # If a log directory is provided in the mounted 'config' dir, use that one
-if [[ -d "/home/cadia/config/Logs" ]] ; then
-    LOGDIR=/home/cadia/config/Logs
-fi
+LOGDIR=/home/cadia/config/Logs
 mkdir -p "${LOGDIR}"
 
 LOGFILE=${LOGDIR}/$(date --utc "+%Y%m%d.%H%M").log
